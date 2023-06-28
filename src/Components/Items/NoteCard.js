4025bf4f538d565f6fa5e0483414f7a7f8ce73b6
Hech-z19 <78944717+Hech-z19@users.noreply.github.com>
2023-06-28 11:16:13 -0400

    Nuevo botón de eliminar en componente de notas
    Incluyendo mejora del responsive y contenedor de texto

--- src/Components/Items/NoteCard.js
+++ src/Components/Items/NoteCard.js
@@ -1,18 +1,37 @@
-import { Card, CardActions, CardContent, Typography } from "@mui/material";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+  IconButton,
+} from "@mui/material";
+import { Delete } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
 
 import translate from "Utils/translate";
 import colors from "Utils/colors";
 import CustomButtom from "Components/Items/CustomButton";
 
-const NoteCard = ({ owner, title, subtitle, content, onClick, src, sx }) => {
+const NoteCard = ({
+  owner,
+  title,
+  subtitle,
+  content,
+  onClick,
+  src,
+  sx,
+  onDelete,
+}) => {
   const navigate = useNavigate();
   return (
     <Card sx={{ width: 275, ...sx }}>
-      <CardContent>
+      <CardContent sx={{ height: "150px" }}>
         <Typography
           sx={{
             fontSize: 14,
+            whiteSpace: "nowrap",
+            textOverflow: "ellipsis",
+            overflow: "hidden",
           }}
           color="text.secondary"
           gutterBottom
@@ -27,20 +46,54 @@ const NoteCard = ({ owner, title, subtitle, content, onClick, src, sx }) => {
               color: colors.secondary,
               cursor: "pointer",
             },
+            whiteSpace: "nowrap",
+            textOverflow: "ellipsis",
+            overflow: "hidden",
           }}
           onClick={() => navigate(src)}
         >
           {title}
         </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        <Typography
+          sx={{
+            mb: 1.5,
+            whiteSpace: "nowrap",
+            textOverflow: "ellipsis",
+            overflow: "hidden",
+          }}
+          color="text.secondary"
+        >
           {subtitle}
         </Typography>
-        <Typography variant="body2">{content}</Typography>
+        <Typography
+          sx={{
+            wordBreak: "break-word",
+            overflow: "hidden",
+            width: "100%",
+            height: "40%",
+            position: "relative",
+            "&::after": {
+              content: '""',
+              position: "absolute",
+              bottom: "0",
+              left: "0",
+              right: "0",
+              height: "30px",
+              background:
+                "linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1))",
+            },
+          }}
+          variant="body2"
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
       </CardContent>
-      <CardActions>
+      <CardActions sx={{ justifyContent: "space-between" }}>
         <CustomButtom onClick={onClick}>
           {translate("CARD_button_edit")}
         </CustomButtom>
+        <IconButton onClick={onDelete}>
+          <Delete />
+        </IconButton>
       </CardActions>
     </Card>
   );
