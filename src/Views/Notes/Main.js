1f6d452c7163d2d7f56a213d489073744123d091
Hech-z19 <78944717+Hech-z19@users.noreply.github.com>
2023-06-28 11:15:16 -0400

    Nuevo snackbar y dialog
    - Snackbar para avisar de creación o edición exitosa
    - Dialog para confirmar eliminación de las notas
    - Cambio del id de la nota por su index

--- src/Views/Notes/Main.js
+++ src/Views/Notes/Main.js
@@ -1,13 +1,39 @@
-import React from "react";
-import { Container, Grid, Typography } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { useState, forwardRef } from "react";
+import {
+  Container,
+  Grid,
+  Typography,
+  Snackbar,
+  Alert,
+  Slide,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+import { useNavigate, useLocation } from "react-router-dom";
 
 import colors from "Utils/colors";
 import translate from "Utils/translate";
 import NoteCard from "Components/Items/NoteCard";
+import CustomButton from "Components/Items/CustomButton";
+
+const Transition = forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const Main = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const [openSuccess, setOpenSuccess] = useState(
+    false || location?.state?.success
+  );
+  const [openDeleteDialog, setOpenDeleteDialog] = useState({
+    state: false,
+    id: "",
+  });
+
   const notes = JSON.parse(localStorage.getItem("notes"));
   const alternativeContainer = {
     display: "flex",
@@ -15,11 +41,30 @@ const Main = () => {
     alignItems: "center",
   };
 
+  const handleClose = () => {
+    setOpenDeleteDialog({ ...openDeleteDialog, id: "", state: false });
+  };
+
+  const handleCloseSnack = () => {
+    setOpenSuccess(false);
+    window.history.replaceState(null, null, window.location.pathname);
+  };
+
+  const deleteNote = () => {
+    let array = [...notes];
+
+    array.splice(openDeleteDialog.id, 1);
+
+    localStorage.setItem("notes", JSON.stringify(array));
+    setOpenDeleteDialog({ ...openDeleteDialog, id: "", state: false });
+  };
+
   return (
     <Container
       maxWidth={false}
       sx={{
         flexGrow: 1,
+        paddingBottom: "10px",
         ...(notes === null || notes.length < 1 ? alternativeContainer : {}),
       }}
     >
@@ -40,21 +85,82 @@ const Main = () => {
           alignItems="center"
           rowSpacing={3}
         >
-          {notes.map((note) => (
-            <Grid item xs={12} sm={6} md sx={{ padding: 0 }}>
+          {notes.map((note, index) => (
+            <Grid item xs={12} sm={6} md sx={{ padding: 0 }} key={index}>
               <NoteCard
                 owner={note?.owner}
                 title={note?.title}
                 subtitle={note?.subtitle}
                 content={note?.content}
-                src={`/view/${note?.id}`}
-                onClick={() => navigate(`/edit/${note?.id}`)}
+                src={`/view/${index}`}
+                onClick={() =>
+                  navigate("/note", {
+                    state: { ...note, id: index, editing: true },
+                  })
+                }
+                onDelete={() =>
+                  setOpenDeleteDialog({
+                    ...openDeleteDialog,
+                    id: index,
+                    state: true,
+                  })
+                }
                 sx={{ margin: "auto" }}
               />
             </Grid>
           ))}
         </Grid>
       )}
+      <Snackbar
+        open={openSuccess}
+        autoHideDuration={2000}
+        onClose={() => handleCloseSnack()}
+      >
+        <Alert
+          onClose={() => handleCloseSnack()}
+          severity="success"
+          sx={{ width: "100%" }}
+        >
+          {location?.state?.text}
+        </Alert>
+      </Snackbar>
+      <Dialog
+        open={openDeleteDialog?.state}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={() => {
+          handleClose();
+        }}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{translate("ROOT_delete_dialog_title")}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            {translate("ROOT_delete_dialog_text")}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <CustomButton
+            onClick={() => {
+              handleClose();
+            }}
+            sx={{
+              textTransform: "none",
+              "&.MuiButtonBase-root": {
+                background: colors.pale,
+              },
+              "&:hover": {
+                background: colors.dark_pale,
+              },
+            }}
+          >
+            {translate("ROOT_delete_dialog_button_1")}
+          </CustomButton>
+          <CustomButton onClick={() => deleteNote()}>
+            {translate("ROOT_delete_dialog_button_2")}
+          </CustomButton>
+        </DialogActions>
+      </Dialog>
     </Container>
   );
 };
